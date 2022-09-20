10e36ba9b96285b1a197ed039acf541337a5d06b
ANJALI KRISHNA <anjalikrishnas456@gmail.com>
2022-09-20 13:40:43 +0530

    commit

---
+++ bin/classcreation.dart
@@ -0,0 +1,26 @@
+///class creation  class classname{ }
+class Mobiles {
+  ///instance variables
+  String? model;   //here ? null aware operator model may or may not be null
+  late int ram ;   //late means we will use variable ram later
+
+///static variable
+static String brand ='REDMI';
+
+void show(){   //user defined function
+  ///local variable
+  String os= "Android";
+  print('Phone OS        :$os');
+  }
+}
+
+void main (){
+  ///object creation syntax > classname objectname = classname();
+  Mobiles phone1 = Mobiles();
+  print('model           :${phone1.model ="Redmi 5"}');
+  print('Ram             :${phone1.ram = 8} GB');
+  print('Brand           :${Mobiles.brand}');
+  phone1.show();
+  print('*************************************');
+
+}
