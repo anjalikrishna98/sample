2c5e01aa90662a28b6867d756b172402b24f9660
ANJALI KRISHNA <anjalikrishnas456@gmail.com>
2022-09-20 10:07:12 +0530

    commit

--- lib/demo1.dart
+++ lib/demo1.dart
@@ -1,4 +1,13 @@
 void main()
 {
-  print('welcome to hello world');
+  String name='anjali';
+  int age=24;
+  String email='abc@gmail.com';
+  double mark=80;
+  int phone=62367890;
+  print('my name is ${name}');
+  print('i am ${age} years old');
+  print('${email}');
+  print('my mark is ${mark}');
+  print('my phone number ${phone}');
 }
