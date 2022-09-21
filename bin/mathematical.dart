d3319aae4d3c12ca7c8c00a4ca23eda0b53bdc22
ANJALI KRISHNA <anjalikrishnas456@gmail.com>
2022-09-21 13:20:52 +0530

    commit

--- bin/mathematical.dart
+++ bin/mathematical.dart
@@ -1,26 +1,22 @@
-void add(){
-  int a=10 ,b= 20 ;
+void add(int a, int b){
   print('sum =  ${a+b}');
 }
 
-void sub (){
-  int a=10, b=7 ;
+void sub (int a, int b){
   print('difference=  ${a-b}');
 }
 
-void mult (){
-  int a =3,b=9 ;
+void mult (int a, int b){
   print('product=   ${a*b}');
 }
 
-void div (){
-  int a=24,b=2 ;
+void div (int a, int b){
   print('value=  ${a/b}');
 }
 
 void main (){
-  add();
-  sub();
-  mult();
-  div();
+  add(10,20);
+  sub(10,4);
+  mult(2,4);
+  div(24,2);
 }
