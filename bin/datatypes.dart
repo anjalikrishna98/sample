ac66e1f3a65ee4ed655f1d94da3f224f08e656bc
ANJALI KRISHNA <anjalikrishnas456@gmail.com>
2022-09-21 13:10:45 +0530

    commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,17 @@
+void main(){
+  String name ='anjali';
+       //  name = 100 ;            /// not support the value of name as integer
+
+  var name1 = 'vishnu';
+     // name1 =1000 ;             ///not supported
+
+  dynamic name2 = 'vikky';
+          name2 =200;          ///dynmaic will support the value
+
+  const String dob = '07/03/1998';
+      //dob=03/07/1998;                 ///value is fixed at compile time
+
+  final String pincode= '689647';
+       // pincode= 689691;             ///value is fixed at runtime
+
+}
---
+++ bin/mathematical.dart
@@ -0,0 +1,26 @@
+void add(){
+  int a=10 ,b= 20 ;
+  print('sum =  ${a+b}');
+}
+
+void sub (){
+  int a=10, b=7 ;
+  print('difference=  ${a-b}');
+}
+
+void mult (){
+  int a =3,b=9 ;
+  print('product=   ${a*b}');
+}
+
+void div (){
+  int a=24,b=2 ;
+  print('value=  ${a/b}');
+}
+
+void main (){
+  add();
+  sub();
+  mult();
+  div();
+}
