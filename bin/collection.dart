a77cc94599d50156c37946352a50f966eff7e624
ANJALI KRISHNA <anjalikrishnas456@gmail.com>
2022-09-30 13:46:15 +0530

    commit

--- bin/collection.dart
+++ bin/collection.dart
@@ -15,4 +15,42 @@ void main () {
    list3 [4]=5;  // ""
   //list3.add(2);  // shows error if growable is false
    print(list3); // 1 1 1 1 1 2 = total length = 6 index = 0 1 2 3 4 5
+
+/// list.unmodifiable
+
+  List <int> list4 = List.unmodifiable([10,20,30,40]);
+  print('list $list4');
+
+  ///list.from
+
+  List list5 = List.from(list1);
+  list5.add(70);
+  print('list3 =$list5');
+
+  /// list.genarate
+  List list6 =List.generate(6, (index) => 1);
+  list6.add(2);
+  print(list6);
+
+  ///list.of
+   List list7 = List.of(list4);
+   print('list7 = $list7');
+
+
+   print('first element in list6 = ${list6.first}');
+   print(list6.contains(1));
+   print(list6.elementAt(3));
+   print(list6.indexOf(2));
+   print(list4.lastIndexOf(30));
+   print(list4.indexOf(30));
+
+
+   for(int index = 0 ; index <list1.length ; index++){
+     print(list1[index]);
+   }
+
+for(dynamic value in list1){
+  print(value);
+}
+
 }
